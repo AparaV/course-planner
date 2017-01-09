e807b87f03d32cbeee21b8b12f92e3a558dfa6fd
Apara <ssapar@gmail.com>
2017-01-08 17:43:20 -0700

    Migrate from inheritance to composition model

--- src/output-templates.js
+++ src/output-templates.js
@@ -2,62 +2,30 @@
 
 import React from 'react';
 
-class Default extends React.Component {
-  render() {
-    return(
-      <div>
-        <h3>
-          Once you have finished entering all your courses, hit submit!
-        </h3>
-      </div>
-    );
-  }
+function Default(props) {
+
+  return(
+    <div>
+      <h3>
+        Once you have finished entering all your courses, hit submit!
+      </h3>
+    </div>
+  );
 };
 
-class Output extends React.Component {
+function Output(props) {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      data: []
-    };
+  var items = props.data.map((value) =>
+    <li key={value.toString()}>
+      {value["course-id"]}, {value["pre-req"]}
+    </li>);
 
-    this.setData = this.setData.bind(this);
-  };
-
-  setData(data) {
-    let temp = [];
-    for (let key in data){
-      temp.push(data[key]);
-    }
-    this.setState({
-      data: temp
-    }, function(){});
-  }
-
-  componentDidMount(){
-    this.setData(this.props.data);
-  }
-
-  componentWillUnmount(){
-    this.setState({
-      data: []
-    });
-  }
-
-  render() {
-    var items = this.state.data.map((value) =>
-      <li key={value.id}>
-        {value["course-id"]}, {value["pre-req"]}
-      </li>);
-
-    return(
-      <div>
-        <h3>Hello</h3>
-        <ul>{items}</ul>
-      </div>
-    );
-  }
+  return(
+    <div>
+      <h3>Hello</h3>
+      <ul>{items}</ul>
+    </div>
+  );
 };
 
 module.exports = {
